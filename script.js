ec4aa4142ec8539e2fef05053666f62f0d2e2930
Yash pareek <134516657+yashpp221@users.noreply.github.com>
2024-05-07 22:28:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Function to open the popup
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+// Open the popup when the page loads
+window.onload = openPopup;
